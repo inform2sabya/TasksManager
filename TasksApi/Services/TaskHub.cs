2c66265be06f28eab2ccc5a15a00f5d07687b5ad
Sabyasachi Maity <inform.sabya@gmail.com>
2024-11-27 16:42:40 +0100

    Restructured folders

---
+++ TasksApi/Services/TaskHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using TasksApi.Repository.Models;
+
+namespace TasksApi.Services
+{
+    public class TaskHub : Hub<ITaskHubClient>
+    {
+        /// <summary>
+        /// Broadcast updates when tasks are created/updated/deleted
+        /// </summary>
+        public async Task SendUpdatedTask(TaskItem updatedTask)
+        {
+            await Clients.All.SendTask(updatedTask);
+        }
+    }
+}
